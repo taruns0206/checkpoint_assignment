1bdafff1d5dab2734146c365f743f29433a8de5d
tarun.soni <taruns020693@gmail.com>
2025-12-17 22:51:37 +0530

    added forwards

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -8,17 +8,23 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "s3-origin"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
     viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
   }
 
   viewer_certificate {
     cloudfront_default_certificate = true
   }
 
-  # REQUIRED block to fix the error
   restrictions {
     geo_restriction {
       restriction_type = "none"
