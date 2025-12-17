26fe986c727a14619e9bad817af750cce5ff3edb
tarun.soni <taruns020693@gmail.com>
2025-12-17 22:49:11 +0530

    corrected origin

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -27,3 +27,11 @@ resource "aws_cloudfront_distribution" "this" {
 
   tags = var.tags
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
